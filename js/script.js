107365404f41057a8cbc4e9ca7e811c406f66c20
WANGEUNYOUNG <youngyoung0724@naver.com>
2022-02-22 22:08:45 +0900

    last

--- js/script.js
+++ js/script.js
@@ -216,7 +216,7 @@ $(document).ready(function(){
         explain : "HTML, CSS, JQUERY, JAVASCRIPT, GRID를 이용했습니다.",
         version: "반응형으로 제작되었습니다.",
         version2 : "",
-        link : "https://youngyoung0724.github.io/MovePoster/"
+        link : "https://youngyoung0724.github.io/Movieposter/"
         }, 
 
         {img : "web_img10.png",
